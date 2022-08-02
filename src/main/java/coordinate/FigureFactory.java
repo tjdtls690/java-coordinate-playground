df4459bc106f16b3ac5c7e61d2f2e845351c4361
JunH <tjdtls690@gmail.com>
2022-08-02 23:08:28 +0900

    :art: refactor : 연습문제 풀이

--- src/main/java/coordinate/FigureFactory.java
+++ src/main/java/coordinate/FigureFactory.java
@@ -1,21 +1,23 @@
 package coordinate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.function.Function;
 
 public class FigureFactory {
-    static Figure getInstance(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
-            return new Line(points);
+    private static final HashMap<Integer, Function<List<Point>, Figure>> hashMap = new HashMap<>();
+    
+    static {
+        hashMap.put(Line.LINE_POINT_SIZE, Line::new);
+        hashMap.put(Triangle.TRIANGLE_POINT_SIZE, Triangle::new);
+        hashMap.put(Rectangle.RECTANGLE_POINT_SIZE, Rectangle::new);
+    }
+    
+    static Figure getInstance(List<Point> points) throws IllegalArgumentException {
+        if (points == null) {
+            throw new IllegalArgumentException("유효하지 않은 도형입니다.");
         }
-
-        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
-            return new Triangle(points);
-        }
-
-        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
-            return new Rectangle(points);
-        }
-
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+        
+        return hashMap.get(points.size()).apply(points);
     }
 }
